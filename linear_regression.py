e2c0c33d6d3536c9371cd96858f7bef55cedd84a
Nick Blum <NPB88@SCDMH.ORG>
2018-07-20 13:57:36 -0400

    Add actual regression line and custom regression line to graph

--- linear_regression.py
+++ linear_regression.py
@@ -3,16 +3,27 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
 
-df = pd.read_csv("sr27abxl/ABBREV.csv")
+#Get all dairy items from dataset. Note: Dairy codes are in the 1000's range
+df = pd.read_csv("sr27abxl/ABBREV.csv").query('1000 <= NDB_No < 1999').dropna()
 
-df_poly_chol = df[['FA_Poly_(g)','Cholestrl_(mg)']]
+# open graph, plot data points
+sns.set_style("darkgrid")
+g = sns.FacetGrid(df, size = 6)
+g = g.map(plt.scatter, "FA_Mono_(g)", "Cholestrl_(mg)", edgecolor="w")
+g = (g.set_axis_labels('Polyunsaturated Fat (g)','Cholesterol (mg)'))
 
-X_plot = np.linspace(0, 7, 100)
-Y_plot = 10*X_plot+5
+# get actual regression line
+gradient, intercept, r_value, p_value, std_err = linregress(df['FA_Mono_(g)'].tolist(),df['Cholestrl_(mg)'].tolist())
+reg_x=np.linspace(0,8,100)
+reg_y=gradient*reg_x+intercept
+plt.plot(reg_x,reg_y)
 
-g = sns.FacetGrid(df_poly_chol, size = 6)
-g = g.map(plt.scatter, "FA_Poly_(g)", "Cholestrl_(mg)", edgecolor="w")
+# get custom regression line
+X_plot = np.linspace(0, 8.5, 100)
+Y_plot = 20*X_plot+30
 plt.plot(X_plot, Y_plot, color='r')
-plt.show()
 
+
+plt.show()
