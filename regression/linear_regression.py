ebe315823ee07c81441777b0ccc57ca421f3ee01
Nick Blum <blum.nick@gmail.com>
2018-09-11 08:41:55 -0400

    Add docstring

--- regression/linear_regression.py
+++ regression/linear_regression.py
@@ -5,4 +5,11 @@ def gradient_descent(X,y,theta,regularize):
     pass
 
 def find_curve(X=0,y=0,alpha=100):
+    """
+        Need to include docstring here. A brief explanation of the function
+
+        X: A brief explanation of this variable
+        y: A brief explanation of this variable
+        alpha: This one too
+    """
     print('Finding curve')
