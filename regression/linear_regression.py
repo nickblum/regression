e7a8f89d0b77449ab3a125582b627f9d99146046
Nick Blum <blum.nick@gmail.com>
2018-11-17 07:35:09 -0500

    Added instructions to use numpy

--- regression/linear_regression.py
+++ regression/linear_regression.py
@@ -6,6 +6,8 @@ def gradient_descent(X,y,theta,regularize):
 
 def find_curve(X=0,y=0,alpha=100):
     """
+        NOTE TO SELF: USE numpy -- it's written in C and a zillion times faster than python functions for arrays and such
+        
         Need to include docstring here. A brief explanation of the function
 
         X: A brief explanation of this variable
